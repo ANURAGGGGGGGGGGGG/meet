a8cd95db1d902822c3c73039c79b420231e7a6bb
ANURAGGGGGGGGGGGG <anuragmahanta730@gmail.com>
2026-05-21 10:46:06 +0530

    adding TURN

--- hooks/useWebRTC.ts
+++ hooks/useWebRTC.ts
@@ -7,7 +7,15 @@ import { getSocket } from "@/lib/socket";
 const ICE_SERVERS = {
   iceServers: [
     { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    {
+      urls: [
+        "turn:openrelay.metered.ca:80",
+        "turn:openrelay.metered.ca:443",
+        "turn:openrelay.metered.ca:443?transport=tcp",
+      ],
+      username: "openrelayproject",
+      credential: "openrelayproject",
+    },
   ],
 };
 
